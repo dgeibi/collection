e1136ac9e3b73119e872ad72adb7a5a8dd5f32ec
dgeibi <png.inside@gmail.com>
2016-12-03 20:11:42 +0800

    queue: Q.front is not empty

--- data-structures/queue/queue.cpp
+++ data-structures/queue/queue.cpp
@@ -2,10 +2,7 @@
 
 void InitQueue_LQ(LQueue& Q) {
   /* 构造一个空队列Q */
-  Q.front = Q.rear = (LQNode *)malloc(sizeof(LQNode));
-
-  if (!Q.front) exit(OVERFLOW);
-  Q.front->next = NULL;
+  Q.front = Q.rear = NULL;
 }
 
 void DestroyQueue_LQ(LQueue& Q) {
@@ -16,28 +13,13 @@ void DestroyQueue_LQ(LQueue& Q) {
     free(Q.front);
     Q.front = Q.rear;
   }
+  Q.front = Q.rear = NULL;
 }
 
-void ClearQueue_LQ(LQueue& Q) {
-  /* 将Q清为空队列 */
-  QueuePtr p, q;
-
-  Q.rear        = Q.front;
-  p             = Q.front->next;
-  Q.front->next = NULL;
-
-  while (p)
-  {
-    q = p;
-    p = p->next;
-    free(q);
-  }
-}
-
-Status IsEmpty_LQ(LQueue Q) {
+bool IsEmpty_LQ(LQueue Q) {
   /* 若Q为空队列，则返回TRUE，否则返回FALSE */
-  if (Q.front->next == NULL) return TRUE;
-  else return FALSE;
+  if (Q.front == NULL) return true;
+  else return false;
 }
 
 int QueueLength_LQ(LQueue Q) {
@@ -52,17 +34,19 @@ int QueueLength_LQ(LQueue Q) {
     i++;
     p = p->next;
   }
+
+  if (Q.rear != Q.front) {
+    i++;
+  }
+
   return i;
 }
 
 Status GetHead_LQ(LQueue Q, QElemType& e) {
   /* 若队列不空，则用e返回Q的队头元素，并返回OK，否则返回ERROR */
-  QueuePtr p;
 
   if (Q.front == Q.rear) return ERROR;
-
-  p = Q.front->next;
-  e = p->data;
+  e = Q.front->data;
   return OK;
 }
 
@@ -72,10 +56,16 @@ void EnQueue_LQ(LQueue& Q, QElemType e) {
 
   if (!p) /* 存储分配失败 */
     exit(OVERFLOW);
-  p->data      = e;
-  p->next      = NULL;
-  Q.rear->next = p;
-  Q.rear       = p;
+  p->data = e;
+  p->next = NULL;
+
+  if (NULL == Q.front) {
+    Q.front = p;
+  }
+  else {
+    Q.rear->next = p;
+  }
+  Q.rear = p;
 }
 
 Status DeQueue_LQ(LQueue& Q, QElemType& e) {
@@ -84,11 +74,11 @@ Status DeQueue_LQ(LQueue& Q, QElemType& e) {
 
   if (IsEmpty_LQ(Q)) return ERROR;
 
-  p             = Q.front->next; /* 指向头结点 */
-  e             = p->data;
-  Q.front->next = p->next;       /* 摘下头节点 */
+  p       = Q.front; /* 指向头结点 */
+  e       = p->data;
+  Q.front = p->next; /* 摘下头节点 */
 
-  if (Q.rear == p) Q.rear = Q.front;
+  if (Q.rear == p) Q.rear = NULL;
   free(p);
   return OK;
 }
@@ -97,7 +87,7 @@ void Traverse_LQ(LQueue Q, void (*vi)(QElemType)) {
   /* 从队头到队尾依次对队列Q中每个元素调用函数vi() */
   QueuePtr p;
 
-  p = Q.front->next;
+  p = Q.front;
 
   while (p)
   {
