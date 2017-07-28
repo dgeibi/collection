9e14fb1602284188b66383567714282b2a092eff
dgeibi <png.inside@gmail.com>
2017-07-28 21:17:32 +0800

    add react/component/

---
+++ react/component/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from 'react-dom';
+import SearchSelect from './components/SearchSelect';
+
+function App() {
+  return (
+    <div>
+      <div className="wrapper">
+        <section>
+          <SearchSelect
+            items={['a', 'b', 'c']}
+          />
+        </section>
+      </div>
+      <style jsx>{`
+        .wrapper {
+          max-width: 800px;
+          margin: 0 auto;
+        }
+      `}
+      </style>
+    </div>
+  );
+}
+
+render(<App />, document.getElementById('root'));
