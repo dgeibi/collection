540dc6fcd8b54ea5f25b7efecd3eb6c304a317c9
dgeibi <png.inside@gmail.com>
2017-11-29 22:05:22 +0800

    (algorithm): improve catchRabbit

--- algorithm/fox-catch-rabbit/catchRabbit.js
+++ algorithm/fox-catch-rabbit/catchRabbit.js
@@ -1,17 +1,14 @@
-const THRESHOLD = 1e3
-const INIT_OFFSET = 1
-const INIT_CURRENT = 0
 function catchRabbit(number) {
   const viewed = new Array(number)
-  let current = INIT_CURRENT
-  let offset = INIT_OFFSET
+  let current = 0
+  let offset = 1
   let count = 0
-  do {
+  while (count < number) {
     count += 1
     viewed[current] = true
     offset = (offset + 1) % number
     current = (current + offset) % number
-  } while (count < THRESHOLD && !(current === INIT_CURRENT && offset === INIT_OFFSET))
+  }
   const notViewed = []
   for (let i = 0; i < viewed.length; i += 1) {
     if (!viewed[i]) notViewed.push(i + 1)
--- algorithm/fox-catch-rabbit/index.js
+++ algorithm/fox-catch-rabbit/index.js
@@ -1,4 +1,5 @@
 const assert = require('assert')
 const catchRabbit = require('./catchRabbit')
 
+assert.deepStrictEqual(catchRabbit(13), [4, 5, 7, 9, 11, 12])
 assert.deepStrictEqual(catchRabbit(10), [2, 4, 7, 9])
